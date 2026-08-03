911ac09f700df85efe088ba2cc572d352beae1d5
Leart Bajrami <bajramileart6@gmail.com>
2026-08-03 11:30:59 +0200

    feat: enhance TabsLayout with NativeTabs and add appointments screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,65 @@
 import { useAuth } from "@clerk/expo";
-import { Redirect, Tabs } from "expo-router";
+import { Redirect } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
-export default function TabsLayout() {
-  const { isLoaded, isSignedIn } = useAuth({
+import { brandColors } from "@/theme/colors";
+
+export default function TabsLayout(){
+  const {isLoaded, isSignedIn} = useAuth({
     treatPendingAsSignedOut: false,
   });
 
-  if (!isLoaded) {
+  if(!isLoaded){
     return null;
   }
 
-  if (!isSignedIn) {
-    return <Redirect href="/(auth)/sign-in" />;
+  if(!isSignedIn){
+    return <Redirect href={"/(auth)/sign-in"}/>;
   }
 
-  return <Tabs screenOptions={{headerShown:false}}/>;
+  return (
+    <NativeTabs tintColor={brandColors.blue}>
+      <NativeTabs.Trigger name="index">
+
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default:"house",
+            selected:"house.fill"
+          }}
+          md="home"
+        />
+      
+        <NativeTabs.Trigger.Label>
+          Ballina
+        </NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="appointments">
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default:"calendar",
+            selected:"calendar.circle.fill"
+          }}
+          md="calendar_month"
+        />
+        <NativeTabs.Trigger.Label>
+          Terminet
+        </NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "person",
+            selected: "person.fill",
+          }}
+          md="person"
+        />
+
+        <NativeTabs.Trigger.Label>
+          Profili
+        </NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  )
 }
